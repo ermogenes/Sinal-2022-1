80270fc4a6ea7b49f85de1db5964a9dd170bd52d
Ermogenes Palacio <ermogenes.palacio@etec.sp.gov.br>
2022-04-01 21:59:07 -0300

    Finaliza exemplo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+// if (numeroDigitado == 0)
+// {
+//     Console.WriteLine("Zero");
+// }
+// else
+// {
+//     if (numeroDigitado < 0)
+//     {
+//         Console.WriteLine("Negativo");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Positivo");
+//     }
+// }
+
+if (numeroDigitado < 0)
+{
+    Console.WriteLine("Negativo");
+}
+else if (numeroDigitado > 0)
+{
+    Console.WriteLine("Positivo");
+}
+else
+{
+    Console.WriteLine("Zero");
+}
